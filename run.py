ce1b32b8cab04c7797af8f4e87343970eaee9b00
katherine wadge-berrospi <katherinewadge@gmail.com>
2025-01-11 16:34:52 +0000

    Added a parrameter that sets the number of ships placed. It also now includes a while loop to place them, ensuring they don't overlap.

--- run.py
+++ run.py
@@ -5,12 +5,21 @@
 board_size = 6
 max_turns = 15
 
-def create_board():
+def create_board(num_ships = 4):
     """
-    Creates the initial board that the game is played on
+    Creates the initial board that the game is played on. 
+    Parameters include the number of ships that need to be placed on the grid.
+    Returns the board and a list of the ship positions.
     """
     board = [["O" for _ in range(board_size)] for _ in range(board_size)]
-    row = random.randint(0, board_size -1)
-    col = random.randint(0, board_size -1)
-    board[row][col] = "#"
-    return board, (row, col)
+    ship_positions = []
+
+    while len(ship_positions) < num_ships:
+        row = random.randint(0, board_size -1)
+        col = random.randint(0, board_size -1)
+    
+    if (row, col) not in ship_positions:
+        board[row][col] = "#"
+        ship_positions.append((row, col))
+
+    return board, ship_positions
