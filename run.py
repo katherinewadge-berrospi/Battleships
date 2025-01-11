bd661b51daba3897b4fad15020dd535152cd488f
katherine wadge-berrospi <katherinewadge@gmail.com>
2025-01-11 16:25:50 +0000

    The create_board function now returns '#' in the place of a ship and added a maximum number of turns

--- run.py
+++ run.py
@@ -3,8 +3,14 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 board_size = 6
+max_turns = 15
 
 def create_board():
+    """
+    Creates the initial board that the game is played on
+    """
     board = [["O" for _ in range(board_size)] for _ in range(board_size)]
     row = random.randint(0, board_size -1)
     col = random.randint(0, board_size -1)
+    board[row][col] = "#"
+    return board, (row, col)
