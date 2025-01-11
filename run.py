d5433eecd18bd3c4c040379f7fd7aeb312461282
katherine wadge-berrospi <katherinewadge@gmail.com>
2025-01-11 16:16:08 +0000

    Entered initial board size and started function to create the starting board

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+board_size = 6
+
+def create_board():
+    board = [["O" for _ in range(board_size)] for _ in range(board_size)]
+    row = random.randint(0, board_size -1)
+    col = random.randint(0, board_size -1)
