90d2cbe19d0c6db27d503b1a646ad74ee7042536
katherine wadge-berrospi <katherinewadge@gmail.com>
2025-01-11 16:51:31 +0000

    Imported random and debugged while loop. Prints out the board successfully.

--- run.py
+++ run.py
@@ -2,10 +2,12 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+from random import randint
+
 board_size = 6
 max_turns = 15
 
-def create_board(num_ships = 4):
+def create_board(num_ships = 5):
     """
     Creates the initial board that the game is played on. 
     Parameters include the number of ships that need to be placed on the grid.
@@ -15,11 +17,18 @@ def create_board(num_ships = 4):
     ship_positions = []
 
     while len(ship_positions) < num_ships:
-        row = random.randint(0, board_size -1)
-        col = random.randint(0, board_size -1)
+        row = randint(0, board_size -1)
+        col = randint(0, board_size -1)
     
-    if (row, col) not in ship_positions:
-        board[row][col] = "#"
-        ship_positions.append((row, col))
+        if (row, col) not in ship_positions:
+            board[row][col] = "#"
+            ship_positions.append((row, col))
 
     return board, ship_positions
+
+board, ship_positions = create_board()
+
+for row in board:
+    print(" ".join(row))
+
+print("Ship positions: ", ship_positions)
